9f2edc6051d085e8f7b31f39dc9fd24ece9da2b5
Irina Lyashenko <iricelwo@gmail.com>
2015-12-02 14:10:19 +0200

    changed first script

--- GoIT/js/lesson10-11/js/script.js
+++ GoIT/js/lesson10-11/js/script.js
@@ -1,13 +1,15 @@
 // task #1
 
-var myBase = prompt('Введите число');
-var myExponent = prompt('Введите степень');
+var myBase = + prompt('Введите число');
+var myExponent = + prompt('Введите степень');
 
 function pow(myBase, myExponent) {
-	var result;
-	result = Math.pow(myBase, myExponent);
+		var result = 1;
+		for (i=0; i < myExponent; i++) {
+		result = myBase * result
+		};
 	return result;
-}
+};
 
 var calculationResult = pow(myBase, myExponent);
 console.log('Ваш результат = ', calculationResult);
