60171404877e7389764fbe59b2672fcd329a21e5
Irina Lyashenko <iricelwo@gmail.com>
2015-12-03 19:17:00 +0200

    added 2nd method

--- GoIT/js/lesson10-11/js/script2.js
+++ GoIT/js/lesson10-11/js/script2.js
@@ -8,11 +8,29 @@ for (var i = 0; i <5; i++) {
 	arr[i] = name;
 }
 
+console.log(arr);
+
 var yourName = prompt('Enter your name');
 
+// method #1
 if (arr.indexOf(yourName) != -1) {
 	alert(yourName + ', you entered successfully');
 } else {
 	alert("you don\'t have permission")
 }
+
+// method #2
+// for (var i = 0; i < 5; i++) {
+// 	var check = 0;
+// 	check = check + (yourName === arr[i]);
+// }
+
+// console.log(check);
+
+// if (check > 0) {
+// 	alert(yourName + ', you entered successfully');
+// } else {
+// 	alert("you don\'t have permission")
+// }
+
 
