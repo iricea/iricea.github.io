dead20a62b77e5eeb17276c39cfd710fe8ad497f
Irina Lyashenko <iricelwo@gmail.com>
2015-12-15 20:01:01 +0200

    editions
    moved styles from script to css file, rewritten function

--- GoIT/js/lesson12-13/js/script.js
+++ GoIT/js/lesson12-13/js/script.js
@@ -1,76 +1,58 @@
-var body = document.querySelector('body');
-body.style.fontFamily = 'Arial';
-body.style.fontSize = '18px';
-
-
 var app = {
 	createElement: function(params) {
 		var element = document.createElement(params.tagName)
-
-		if (params.inputType) {
-			element.setAttribute('type', params.inputType);
-		}
 
 		if (params.className) {
 			element.className = params.className;
 		}
 
+
 		if (params.content) {
 			element.innerHTML = params.content;
 		}
 
-		if (params.parentElement) {
-			params.parentElement.appendChild(element);
-		}
 		return element;
-	},
+	}
+};
 
-	generateQuestions: function (questionsAmount, answersAmount) {
-		
-		for (var i = 1; i <= questionsAmount; i++) {
-
-			var li = this.createElement({
-				tagName: 'li',
-	      content: 'Вопрос №' + i,
-	      parentElement: ol
-			});
-			li.style.padding = '10px';
-
-			for (var j = 1; j <= answersAmount; j++) {
-
-        var label = this.createElement({
-          tagName: 'label',
-          content: '<input type="checkbox"> Вариант ответа №' + j,
-          parentElement: ol
-        });
-        label.style.display = 'block';
-        label.style.fontSize = '15px';
-        label.style.fontWeight = 'normal';
-        label.style.paddingLeft = '11px';
-      }
-    }
-  }
-}
-
-var header = app.createElement({
+var $header = app.createElement({
 	tagName: 'header',
-	content: 'Тест по программированию',
-	parentElement: body
+	content: 'Тест по программированию'
 });
 
-header.style.textAlign = 'center';
+document.body.appendChild($header);
 
-var ol = app.createElement({
-	tagName: 'ol',
-	parentElement: body
+var $questionBlock = app.createElement({
+	tagName: 'ol'
 });
 
-app.generateQuestions(3, 3);
+document.body.appendChild($questionBlock);
 
-var button = app.createElement({
+for (var i = 1; i <= 3; i++) {
+	var $question = app.createElement({
+		tagName: 'li',
+		className: 'question',
+		content: 'Вопрос №' + i
+	});
+
+	$questionBlock.appendChild($question);
+
+		for (var j = 1; j <= 3; j++) {
+		var $answer = app.createElement({
+			tagName: 'label',
+			className: 'answer',
+			content: '<input type="checkbox"> Вариант ответа №' + j
+		});
+
+		$question.appendChild($answer);
+	};
+};
+
+var $button = app.createElement({
 	tagName: 'button',
-	inputType: 'button',
 	className: 'btn btn-default center-block',
 	content: 'Проверить мои результаты',
-	parentElement: body
 });
+
+document.body.appendChild($button);
+$button.setAttribute('type', 'submit');
